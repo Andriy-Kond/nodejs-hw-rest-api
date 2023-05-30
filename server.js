4dcb5f472e66f6e67f5f713a3ecbe59b86f01f9d
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-05-30 10:01:27 +0300

    ADD: mongodb+deploy render.com

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const mongoose = require("mongoose");
 // const DB_HOST =
 // 	"mongodb+srv://Andriy-User:knxnEFLO6qBZbGQE@cluster0.9p0p26j.mongodb.net/db-contacts?retryWrites=true&w=majority";
 // const { DB_HOST } = require("./config");
-const { DB_HOST } = process.env;
+// const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 // process.env - це глобальний об'єкт зі змінними оточення поточного пристрою (серверу/комп'ютеру).
 // console.log("process.env :>> ", process.env);
@@ -20,12 +21,14 @@ const { DB_HOST } = process.env;
 // 2. Замість config.js створюємо файл .env. Це простий текстовий файл з секретними даними. Тому одразу додаємо його у gitignore. Плюс .env.local (хоча це більше стосується фрронтенду)
 // 3. Записуємо у .env змінну і значення через дорівнює без лапок і пробілів: DB_HOST=mongodb+srv://Andriy-User:knxnEFLO6qBZbGQE@cluster0.9p0p26j.mongodb.net/db-contacts?retryWrites=true&w=majority Якщо тре нову змінну оточення, то пишемо її з нового рядку. Наприклад, PORT=3000 А config.js можна тепер видалити.
 // 4. Додаємо дані з файлу .env у process.env у app.js (через dotenv)
+// 5. Через те, що .env прописаний у gitignore, то девопс не буде знати навіть назв змінних оточення. Тому для нього треба зробити окремий файл з назвою .env.example (або як домовитесь з девопсом)). Значення до цих назв девопс потім підставить.
 
 mongoose.set("strictQuery", true); // З сьомої версії Mangoose воно false за замовчуванням.
 
 mongoose
 	.connect(DB_HOST) // повертає проміс
-	.then(() => app.listen(3000))
+	// .then(() => app.listen(3000)) // PORT вже прописаний у process.env
+	.then(() => app.listen(PORT))
 	.catch((error) => {
 		console.log(error.message);
 		process.exit(1); // Ця команда закриває запущені процеси. Якщо до того як під'єднатись до бази щось запустили, то воно буде закрито. "1" - означає "закрити з невідомою помилкою"
