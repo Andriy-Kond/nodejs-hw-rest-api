8714aaaccadc7f375e7b48f9e023454d0626c51e
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-06-07 21:16:02 +0300

    UPD: Jimp

--- controllers/users.js
+++ controllers/users.js
@@ -86,16 +86,24 @@ const Jimp = require('jimp');
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
+  // Щоби перемістити файл у fs використовується метод rename()
+  // Перший аргумент - старий шлях з ім'ям. Другий - новий шлях з ім'ям
+  // await fs.rename('./temp/Screenshot_1.jpg', './public/books/Screenshot_1.jpg');
+
+  // Старий шлях записаний у req.file.path. Ім'я файлу - у req.file.originalname
   const { path: tempUpload, originalname } = req.file;
 
   const filename = `${_id}_${originalname}`; // створюємо нове унікальне ім'я, щоби однакові імена файлів від різних юзерів не перезаписували їхні файли, бо всі вони будуть лежати в одній теці
 
-  const resultUpload = path.join(avatarDir, filename);
+  // Створюємо новий шлях - до шляху до теки books (booksDir) додаємо ім'я файлу
+  const resultUpload = path.join(avatarDir, filename); // абсолютний шлях на сервер
 
   await fs.rename(tempUpload, resultUpload); // переміщуємо з tempUpload до resultUpload
 
-  // записуємо цей шлях у базу:
-  const avatarURL = path.join('avatars', filename);
+  // Обробляємо поля з фронтенду - додаємо унікальний id і відносний шлях (відносно адреси сайту), де лежить доданий файл
+  // const newFilePath = path.join('public', 'books', originalname);
+  // записуємо цей шлях у базу
+  const avatarURL = path.join('avatars', filename); // "public" видаляємо, бо він тепер зазначений у middleware app.use(express.static('public'));
 
   await User.findByIdAndUpdate(_id, { avatarURL });
 
@@ -103,15 +111,17 @@ const updateAvatar = async (req, res) => {
   // open a file
   Jimp.read(`${avatarDir}/${filename}`, (err, fileAvatar) => {
     if (err) throw err;
-    console.log('Jimp.read >> filename:', filename);
-    console.log('Jimp.read >> avatarDir:', avatarDir);
+    // console.log('Jimp.read >> fileAvatar:', fileAvatar);
+    // console.log('Jimp.read >> filename:', filename);
+    // console.log('Jimp.read >> avatarDir:', avatarDir);
     fileAvatar
-      .resize(256, 256) // resize
-      .quality(25); // set JPEG quality
-    // .greyscale() // set greyscale
-    // .write(filename); // save
+      .resize(250, 250) // resize
+      .quality(60) // set JPEG quality
+      // .greyscale() // set greyscale
+      .write(`${avatarDir}/${filename}`); // save
   });
 
+  // Повертаємо на фронтенд:
   res.json({ avatarURL });
 };
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -32,4 +32,30 @@ router.patch(
   ctrl.updateAvatar
 );
 
+// // Запис upload.single("cover") означає: ми очикуємо у полі cover один файл, всі інші поля будуть текстовими і їх тре записати у req.body
+// // Тобто - візьми з поля cover файл, збережи його у теці temp, а текстові поля передай у req.body
+// // Якщо ми очікуємо кілька файлів: upload.array("cover", 8) - другий аргумент - максимальна кількість файлів
+// // Якщо очікуємо кілька файлів у різних полях: upload.fields([{ name: "cover", maxCount: 1}, { name: "subcover", maxCount: 2}])
+// app.post('/api/books', upload.single('cover'), async (req, res) => {
+//   console.log('req.body :>> ', req.body);
+//   // req.body :>>  [Object: null prototype] { title: 'girl genius', author: 'Gogilo' }
+//   console.log('req.file :>> ', req.file);
+//   //   req.file :>>  {
+//   //   fieldname: 'cover',
+//   //   originalname: 'Screenshot_1.jpg',
+//   //   encoding: '7bit',
+//   //   mimetype: 'image/jpeg',
+//   //   destination: 'D:\\Programming\\JS-Node-HW\\Module-05\\file-delivery-multer\\temp',
+//   //
+//   //   filename: '095c5e971d252c8becfc0128aefdea6b',
+//   //   path: 'D:\\Programming\\JS-Node-HW\\Module-05\\file-delivery-multer\\temp\\095c5e971d252c8becfc0128aefdea6b',
+//   //
+//   //   або, якщо вказати filename у multerConfig:
+//   //   filename: 'Screenshot_1.jpg',
+//   //   path: 'D:\\Programming\\JS-Node-HW\\Module-05\\file-delivery-multer\\temp\\Screenshot_1.jpg',
+//   //
+//   //   size: 59330
+//   // }
+// });
+
 module.exports = router;
